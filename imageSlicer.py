37b60653cf3b13a3d050a67cc0f544c5288197a7
Jonathan Young <22777164+semperExploro@users.noreply.github.com>
2020-09-12 15:16:23 -0400

    Update imageSlicer.py

--- imageSlicer.py
+++ imageSlicer.py
@@ -1,13 +1,16 @@
 import image_slicer
-numberOfFaces =12
-sliceAmount = int(round(numberOfFaces / 10, 1))+1
-#print(sliceAmount)
+import os
+imageFilePath =r'C:\Users\jonat\Documents\Hophacks\Zoom Sample\Capture.png'
+numberOfFaces = 12
+sliceAmount = int(round(numberOfFaces / 10, 1)) + 1
+# print(sliceAmount)
 
-if(sliceAmount>1and sliceAmount<4):
-   # print(sliceAmount)
-    image_slicer.slice(r'C:\Users\jonat\Documents\Hophacks\Zoom Sample\Capture.png', sliceAmount)
-if(sliceAmount>=5):
-    sliceAmount =5
-    #print(sliceAmount)
-    image_slicer.slice(r'C:\Users\jonat\Documents\Hophacks\Zoom Sample\Capture.png', sliceAmount)
-
+if (sliceAmount > 1 and sliceAmount < 3):
+    # print(sliceAmount)
+    image_slicer.slice(imageFilePath, sliceAmount)
+    os.remove(imageFilePath)
+if (sliceAmount >= 3):
+    sliceAmount = 3
+    # print(sliceAmount)
+    os.remove(imageFilePath)
+    image_slicer.slice(imageFilePath, sliceAmount)
