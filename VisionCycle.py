8d39255b213f7945bd2c1c341ef453db7347c5a1
damianomarsili <70812126+damianomarsili@users.noreply.github.com>
2020-09-12 12:53:25 -0500

    Update VisionCycle.py

--- VisionCycle.py
+++ VisionCycle.py
@@ -2,9 +2,13 @@ import os
 import io
 from google.cloud import vision
 
+"""
+Iterates over all images in a given directory and determines the pan(horizontal) and face(vertical) tilt of
+all faces in each image. At the moment prints to console - will need to create output file.
+"""
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'C:\Users\jonat\Documents\Hophacks\hophacks-52ea2c3c8fe5.json'
-
+client = vision.ImageAnnotatorClient()
 
 def google_vision(image,content):
     image = vision.types.Image(content=content)
@@ -23,12 +27,14 @@ def google_vision(image,content):
         print("Count %d"%x)
         print('')
 
+def iterate_on_dir(imgPath):
+    for img in os.listdir(imgPath):
+        file_name = os.path.abspath(os.path.join(imgPath, img))
+        with io.open(file_name, 'rb') as image_file:
+            content = image_file.read()
+        image = vision.types.Image(content=content)
+        google_vision(image, content)
 
-client = vision.ImageAnnotatorClient()
-imgPath = r'C:\Users\jonat\Documents\Hophacks\Photos'
-for img in os.listdir(imgPath):
-    file_name = os.path.abspath(os.path.join(imgPath,img))
-    with io.open(file_name, 'rb') as image_file:
-        content = image_file.read()
-    image = vision.types.Image(content=content)
-    google_vision(image,content)
+
+if __name__ == "__main__":
+    iterate_on_dir(os.path.join(os.getcwd(),"frames"))
