e2a224859e599f84298d8c609dc579ddfb8eaf9b
German Alvarez <germanalvarez14@gmail.com>
2020-09-12 14:30:23 -0700

    updated correct spelling of Faceior

--- Faceior_Innovo/asgi.py
+++ Faceior_Innovo/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Facieior_Innovo project.
+ASGI config for Faceior_Innovo project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Facieior_Innovo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Faceior_Innovo.settings')
 
 application = get_asgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Facieior_Innovo.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Faceior_Innovo.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
