3ed6a073489bb9c0181dca276aa54ac000339a48
damianomarsili <70812126+damianomarsili@users.noreply.github.com>
2020-09-12 13:44:54 -0500

    Update VisionCycle.py

--- VisionCycle.py
+++ VisionCycle.py
@@ -1,4 +1,5 @@
 import os
+import math
 import io
 from google.cloud import vision
 
@@ -7,8 +8,10 @@ Iterates over all images in a given directory and determines the pan(horizontal)
 all faces in each image. At the moment prints to console - will need to create output file.
 """
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'C:\Users\jonat\Documents\Hophacks\hophacks-52ea2c3c8fe5.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'C:\Users\dmars\Downloads\hophacks-a78f8c494166.json'
+
 client = vision.ImageAnnotatorClient()
+DISTRACT_THRESHOLD = 18.0
 
 def google_vision(image,content):
     image = vision.types.Image(content=content)
@@ -17,24 +20,27 @@ def google_vision(image,content):
 
     likehood = ('Unknown', 'Very Unlikely', 'Unlikely', 'Possibly', 'Likely', 'Very Likely')
 
-    print('Faces:')
     x=0
+    numDistracted = 0
 
     for face in faceAnnotations:
-        print("Pan tilt " + str(face.pan_angle))
-        print("Face tilt "+ str(face.tilt_angle))
         x=x+1
-        print("Count %d"%x)
-        print('')
+        if (abs(face.pan_angle) >= DISTRACT_THRESHOLD):
+            numDistracted += 1
+
+
+    return (numDistracted / x) * 100
 
 def iterate_on_dir(imgPath):
+    percentDistracted = []
     for img in os.listdir(imgPath):
         file_name = os.path.abspath(os.path.join(imgPath, img))
         with io.open(file_name, 'rb') as image_file:
             content = image_file.read()
         image = vision.types.Image(content=content)
-        google_vision(image, content)
+        percentDistracted.append(google_vision(image, content))
 
+    return percentDistracted
 
 if __name__ == "__main__":
-    iterate_on_dir(os.path.join(os.getcwd(),"frames"))
+    print(iterate_on_dir(os.path.join(os.getcwd(),"frames")))
