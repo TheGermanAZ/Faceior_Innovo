aeae58ec01930238e89c9035c61cfc09dedf8267
Damiano Marsili <dmarsil1@jh.edu>
2020-09-12 20:09:48 -0500

    Added upload functionality

--- Faceior_Innovo/urls.py
+++ Faceior_Innovo/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('video_analytics', include('video_analytics.urls'))
+    path('video_analytics/', include('video_analytics.urls')),
 ]
